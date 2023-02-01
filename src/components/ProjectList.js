78184ad6c5fab194b1e928e4e6e3888e760a6ed2
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-02-01 13:13:15 +0300

    Completed Lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,13 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+
+      <div id="project-list">
+      {projects.map((project,id) =>(
+        <ProjectItem name={project.name} key={id} technologies={project.technologies} />
+      ))}
+        </div>
+
     </div>
   );
 }
